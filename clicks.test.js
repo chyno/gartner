51753e4967fee2d991acfd69fae955abd722cef3
JOhn Chynoweth <jchynowe1@yahoo.com>
2019-04-15 22:02:14 -0400

    get lists

--- clicks.test.js
+++ clicks.test.js
@@ -1,10 +1,61 @@
 const getData = require('./index').getData;
+const getHour = require('./index').getHour;
+const byHour = require('./index').byHour;
+const maxClick = require('./index').maxClick;
+const R = require('ramda');
 
 describe('clicks test', () => {
+  
+
+    beforeEach(() => {
+       
+      });
+
+   
     test('it works', async () => {
       const data = await getData();
       expect(data).toBeDefined();
       expect(Array.isArray(data)).toBeTruthy();
     });
+
+    //For each IP within each one hour period, only the most expensive click is placed into the result set.
+    test('get hour object',  () => {
+        const res =  getHour( {timestamp:"3/11/2016 02:02:58"});
+        expect(res).toBe(2);
+
+        const res2 =  getHour( {timestamp:"3/11/2016 04:12:22"});
+        expect(res2).toBe(4);
+        
+      });
+
+      test('get list group by hour',  async () => {
+        const data = await getData();
+
+        const res =  byHour(data);
+        expect(res).toBeTruthy();
+
+        console.log('**** Grouped object *****');
+        //console.log(res);
+        
+      });
+
+      test('get new max click',   () => {
+        const res = maxClick([
+            {
+                id: 0,
+                amount : 1
+            },
+            {
+                id: 1,
+                amount : 3.5
+            },
+            {
+                id: 2,
+                amount : 3.4
+            }
+        ]);
+        expect(res.id).toBe(1);
+        
+      });
    
   })
--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const loadJsonFile = require('load-json-file');
-
+const R = require('ramda');
 
 //The result set should be stored in an array of hashes. Each hash should represent a click. T
 async function getData() {
    return await loadJsonFile('clicks.json');
 }
 
-module.exports = { getData};
+
+ function getHour(obj) {
+     return (new Date(obj.timestamp)).getHours();
+ }
+
+ function findMaxClick(curMax, curItem) {
+     //timestamp
+  if (curMax.amount === curItem.amount) {
+      let maxDate = new Date(curMax.timestamp);
+      let curDate = new Date(curItem.timestamp);
+
+      return maxDate > curDate ? curMax : curItem;
+  }
+  if (curMax.amount > curItem.amount) {
+      return curMax;
+  }
+  else {
+    return curItem;
+  }
+ }
+
+ // (a → String) → [a] → {String: [a]}
+ const byHour = R.groupBy(getHour);
+
+ // ((a, b) → a) → a → [b] → a
+ const maxClick = R.reduce(findMaxClick, {amount: -1});
+
+
+ // Export these for my unit tests
+module.exports = { getData, getHour, byHour, maxClick};
